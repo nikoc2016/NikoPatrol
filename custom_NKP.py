a107dabe43c0a8c5b824ed86db661b8bf05ccd51
NikoC <nikoc@github.com>
2023-07-25 17:29:10 +0800

    NKPMacau

--- custom_NKP.py
+++ custom_NKP.py
@@ -1,7 +1,12 @@
+from PySide2.QtWidgets import QPushButton
+
 import NKP
 from NKP import NKP_Runtime, NKP_Res, NKP_Res_Custom
 from NKP import NKP_Language
 from NKP.Widgets import NKPMainWindow
+from NKP.Widgets.Area_Backup import BackUpArea
+from NKP.Widgets.Area_TempMonitor import TemperatureMonitorArea
+from NikoKit.NikoLib import NKZip
 from NikoKit.NikoStd import NKConst
 from NikoKit.NikoStd.NKVersion import NKVersion
 
@@ -9,7 +14,7 @@ from NikoKit.NikoStd.NKVersion import NKVersion
 def init_hook():
     NKP.Runtime = NKPMRuntime  # Hook On to Diy
     NKP.MainWin = NKPMMainWindow  # Hook On to Diy
-    NKP.name = "NKPatrol-DefaultMod"  # Must Change So AppData Won't Collide
+    NKP.name = "NKPatrol-Macau"  # Must Change So AppData Won't Collide
     NKP.skip_main_win_load = False  # Skipping load main win, do it manually in after_hook()
     NKP.name_short = "NKP"
     NKP.icon_res_name = "NKP.png"
@@ -19,6 +24,9 @@ def init_hook():
     NKP.resource_patch.update(NKP_Res_Custom.res)
     NKP_Language.ZH_CN_Patch.update(
         {
+            "ui_extract_7za": "安装精简版7Z",
+            "minecraft": "我的世界",
+            "son_of_forest": "森林之子",
         }
     )
 
@@ -33,8 +41,19 @@ class NKPMRuntime(NKP_Runtime.NKPRuntime):
 
 class NKPMMainWindow(NKPMainWindow):
     def __init__(self):
-        auto_render_areas = []
-        super().__init__(auto_render_areas=auto_render_areas)
+        self.install_7za_button = QPushButton(NKP.Runtime.Service.NKLang.tran("ui_extract_7za"))
+        auto_render_areas = [
+            TemperatureMonitorArea(),
+            BackUpArea("minecraft", NKPMRuntime.Service.NKLang.tran("minecraft")),
+            BackUpArea("son_of_forest", NKPMRuntime.Service.NKLang.tran("son_of_forest")),
+        ]
+        super().__init__(auto_render_areas=auto_render_areas, w_title=NKP.name)
+        self.button_lay.insertWidget(self.button_lay.count() - 1, self.install_7za_button)
 
     def connect_signals(self):
         super().connect_signals()
+        self.install_7za_button.clicked.connect(self.slot_install_7za)
+
+    def slot_install_7za(self):
+        print("Extracting 7z to temp.")
+        NKZip.prepare_7z_binaries()
